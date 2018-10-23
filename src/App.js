1d0d94000ab976cf2ceffc4b68a4aba2b14b1df7
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-23 10:04:49 +0530

    using new syntax for constructor function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import lottery from './lottery';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = { manager: '' };
-  }
-  
+  state = {
+    manager: ''
+  };
+
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
     this.setState({ manager });
